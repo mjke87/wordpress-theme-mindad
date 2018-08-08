cf820cc789481da74a9a6dedf0a07f43947c912c
Mike Jarrett <mjarrett@raptus.com>
2018-08-08 17:53:51 +0200

    Remove Google Tag Manager Integration

--- header.php
+++ header.php
@@ -12,20 +12,7 @@
 
 <head>
 
-    <?php if (defined('GOOGLE_TAG_MANAGER')) : ?>
-	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','<?php echo GOOGLE_TAG_MANAGER; ?>');</script>
-	<!-- End Google Tag Manager -->
-    <?php endif; ?>
-
 	<title><?php if ( is_single() ) { ?> <?php } ?><?php wp_title(':',true,'right'); ?> <?php bloginfo('name'); ?></title>
-
-    <?php $assets_url = get_template_directory_uri() . '/assets'; ?>
-
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Roboto+Mono|Roboto+Slab" rel="stylesheet">
@@ -35,14 +22,9 @@
 	<?php wp_head(); ?>
 
 </head>
-<body>
+<body <?php body_class(); ?>>
 
-    <?php if (defined('GOOGLE_TAG_MANAGER')) : ?>
-	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo GOOGLE_TAG_MANAGER; ?>"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
-    <?php endif; ?>
+    <?php do_action('wp_body'); ?>
 
 	<div class="container">
 		<div class="top">
