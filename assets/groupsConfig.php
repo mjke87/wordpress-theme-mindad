3963d1cee21340487cd86c704149cc503b6e14ec
Mike Jarrett <mjarrett@raptus.com>
2018-08-13 14:29:59 +0200

    Add groups configuration for compatibilty with assets management

---
+++ assets/groupsConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    'js' => array(
+        '//wp-includes/js/jquery/jquery.js',
+        '//wp-includes/js/jquery/jquery-migrate.min.js',
+        '//wp-content/plugins/wp-featherlight/js/wpFeatherlight.pkgd.min.js',
+    ),
+    'css' => array(
+        //'//wp-content/plugins/gutenberg/build/core-blocks/style.css',
+        '//wp-content/plugins/wp-featherlight/css/wp-featherlight.min.css',
+        '//wp-content/themes/mindad/assets/css/main.css',
+    )
+);
+
+?>
