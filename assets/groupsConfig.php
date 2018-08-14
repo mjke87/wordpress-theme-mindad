76c36e7f580998778a4738314c33fbd368b3389e
Mike Jarrett <mjarrett@raptus.com>
2018-08-14 15:12:05 +0200

    Add comment reply javascript to groups configuration

--- assets/groupsConfig.php
+++ assets/groupsConfig.php
@@ -5,6 +5,7 @@ return array(
         '//wp-includes/js/jquery/jquery.js',
         '//wp-includes/js/jquery/jquery-migrate.min.js',
         '//wp-content/plugins/wp-featherlight/js/wpFeatherlight.pkgd.min.js',
+        '//wp-includes/js/comment-reply.min.js',
     ),
     'css' => array(
         '//wp-content/plugins/wp-featherlight/css/wp-featherlight.min.css',
