c9ee6c5203e2a07fb30ee0335586837bcbb42a83
Mike Jarrett <mjarrett@raptus.com>
2018-08-15 10:35:57 +0200

    Remove unessential functions, moved to separate plugin

--- functions.php
+++ functions.php
@@ -78,19 +78,6 @@ if (!function_exists('mindad_define_accent_color')) {
 add_action('wp_head', 'mindad_define_accent_color', 10);
 
 /**
- * Clean up head
- */
-if (!function_exists('mindad_clean_up_head')) {
-    function mindad_clean_up_head() {
-        remove_action('wp_head', 'wp_generator');
-        remove_action('wp_head', 'wp_no_robots');
-        remove_action('wp_head', 'wp_shortlink_wp_head');
-    }
-}
-add_action('wp_loaded', 'mindad_clean_up_head', 1);
-
-
-/**
  * Append read more link to post excerpts
  */
 if (!function_exists('mindad_append_read_more_link')) {
@@ -146,116 +133,6 @@ if (!function_exists('mindad_display_post_status_as_badges')) {
     }
 }
 add_action('init', 'mindad_display_post_status_as_badges');
-
-/**
- * Deactivate Gutenberg styles
- */
-if (apply_filters('mindad_disable_gutenberg_styles', true)) {
-    add_action('wp_enqueue_scripts', function() {
-        wp_dequeue_style('wp-core-blocks');
-    }, 99);
-}
-
-/**
- * Remove scripts
- */
-if (apply_filters('mindad_disable_jquery', true)) {
-     add_action('wp_enqueue_scripts', function() {
-        remove_action('wp_head', 'wp_print_scripts');
-        remove_action('wp_head', 'wp_print_head_scripts', 9);
-        remove_action('wp_head', 'wp_enqueue_scripts', 1);
-    });
-}
-
-/**
- * Disable emoji
- */
-if (apply_filters('mindad_disable_emoji', true)) {
-    remove_action('wp_head', 'print_emoji_detection_script', 7);
-    remove_action('wp_print_styles', 'print_emoji_styles');
-}
-
-/**
- * Disable embed
- */
-if (apply_filters('mindad_disable_embed', true)) {
-    add_action('init', function () {
-        // Remove the REST API endpoint.
-        remove_action('rest_api_init', 'wp_oembed_register_route');
-
-        // Turn off oEmbed auto discovery.
-        add_filter('embed_oembed_discover', '__return_false');
-
-        // Don't filter oEmbed results.
-        remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
-
-        // Remove oEmbed discovery links.
-        remove_action('wp_head', 'wp_oembed_add_discovery_links');
-
-        // Remove oEmbed-specific JavaScript from the front-end and back-end.
-        remove_action('wp_head', 'wp_oembed_add_host_js');
-        add_filter('tiny_mce_plugins', function ($plugins) {
-            return array_diff($plugins, array('wpembed'));
-        });
-
-        // Remove all embeds rewrite rules.
-        add_filter('rewrite_rules_array', function ($rules) {
-            foreach($rules as $rule => $rewrite) {
-                if(false !== strpos($rewrite, 'embed=true')) {
-                    unset($rules[$rule]);
-                }
-            }
-            return $rules;
-        });
-
-        // Remove filter of the oEmbed result before any HTTP requests are made.
-        remove_filter('pre_oembed_result', 'wp_filter_pre_oembed_result', 10);
-    }, 50);
-}
-
-/**
- * Clean up classes using a blacklist
- */
-function mindad_clean_classes($wp_classes, $extra_classes) {
-	$blacklist = apply_filters('mindad_body_class_blacklist', array(
-		'post-template-default',
-		'single-format-standard',
-		'wp-featherlight-captions',
-		'/postid-.+/',
-		'page-template-default',
-		'/page-id-.+/',
-		'page-template',
-		'/page-template-(.+)-php/',
-		'page-parent',
-		'/category-.+/',
-        '/post-.+/',
-        'type-post',
-        'status-publish',
-        'format-standard',
-        'hentry',
-        'logged-in',
-	));
-	foreach($wp_classes as $i => $class) {
-		foreach($blacklist as $block) {
-			if ($class == $block) {
-				unset($wp_classes[$i]);
-			} else if (substr($block, 0, 1) == '/' && preg_match($block, $class)) {
-				unset($wp_classes[$i]);
-			}
-		}
-	}
-    return array_merge($wp_classes, (array) $extra_classes);
-}
-
-/**
- * Clean up body classes
- */
- add_filter('body_class', 'mindad_clean_classes', 10, 2);
-
-/**
- * Clean up post classes
- */
-add_filter('post_class', 'mindad_clean_classes', 10, 2);
 
 /**
  * Fix comment reply function if YOAST is installed.
