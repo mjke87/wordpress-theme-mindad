78c59e13922efd696be46f6843cd6c15eadff025
Mike Jarrett <mjarrett@raptus.com>
2018-09-05 09:15:13 +0200

    Init the post on in the search page template to fix a display bug

--- searchpage.php
+++ searchpage.php
@@ -15,6 +15,8 @@ Template Name: Search Page
 
 <?php get_header(); ?>
 
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
 		<h2><?php the_title(); ?></h2>
 
 		<article class="post">
@@ -32,5 +34,7 @@ Template Name: Search Page
 			</p>
 
 		</article>
+
+		<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
